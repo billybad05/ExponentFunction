ea8fa8b079f6af2ae941ae9cf938b5a70994ae15
Billy Dotson <billybad5@gmail.com>
2022-09-22 16:06:14 -0400

    Add project files.

---
+++ ExponentFunction/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Exponent Function!");
+
+Console.WriteLine(GetPow(4, 3));
+
+static int GetPow(int baseNum, int powNum)
+
+{
+    int result = 1;
+
+    for(int i = 0; i < powNum; i++) {
+        result = result * baseNum;
+    }
+
+    return result;
+}
